bf1c6764937200434eede000e790cab4db852552
Michael Wasser <Michael@ElasticBox.com>
2011-07-29 19:48:25 -0700

    updating redirect url to be for legal docs and use im feeling lucky

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/:case_id' do 
-  redirect "http://scholar.google.com/scholar?q=#{params[:case_id]}"
+  redirect "http://scholar.google.com/scholar?hl=en&q=#{params[:case_id]}&as_sdt=2%2C22&as_ylo=&as_vis=0&btnI=I%27m+Feeling+Lucky"
 end
