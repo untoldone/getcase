b79ea1124bd62680926ede23287855202401e11f
Michael Wasser <Michael@ElasticBox.com>
2011-07-29 20:14:36 -0700

    adding ?s= search

--- main.rb
+++ main.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
+def case_redirect(term)
+  redirect "http://scholar.google.com/scholar?hl=en&q=#{term}&as_sdt=2%2C22&as_ylo=&as_vis=0&btnI=I%27m+Feeling+Lucky"
+end
+
 get '/:case_id' do 
-  redirect "http://scholar.google.com/scholar?hl=en&q=#{params[:case_id]}&as_sdt=2%2C22&as_ylo=&as_vis=0&btnI=I%27m+Feeling+Lucky"
+  case_redirect params[:case_id]
+end
+
+get '/' do
+  case_redirect params[:s]
 end
